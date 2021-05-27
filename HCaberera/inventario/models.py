0f153fc17c4328549d4afb019ab87db56054f30c
Benjamín Navarro <be.navarroa@duocuc.cl>
2021-05-27 04:56:31 -0400

    final

--- HCaberera/inventario/models.py
+++ HCaberera/inventario/models.py
@@ -6,8 +6,17 @@ from django.db import models
 
 class Maquina(models.Model):
    idMaquina = models.IntegerField(primary_key=True,verbose_name='Id de maquina')
-   marcaMaquina = models.CharField(max_length=45,verbose_name='Nombre de la marca')
-   modeloMaquina = models.CharField(max_length=45,verbose_name='Modelo de la maquina')
+   marcaMaquina = models.CharField(max_length=50,verbose_name='Nombre de la maquina')
+   modeloMaquina = models.CharField(max_length=50,verbose_name='Modelo de la maquina')
 
    def __str__(self):
        return self.marcaMaquina
+
+class Repupesto(models.Model):
+   idRepuesto = models.IntegerField(primary_key=True,verbose_name='Id de repuesto')
+   nombreRepuesto = models.CharField(max_length=20, verbose_name='Nombre repuesto')
+   maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE)
+ 
+   def __str__(self):
+
+       return self.nombreRepuesto
